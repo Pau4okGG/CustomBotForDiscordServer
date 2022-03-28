bcdcb9ae247d390eafd1edd08042b6a85cb85395
Pau4ok <62987332+Pau4okGG@users.noreply.github.com>
2022-03-28 16:17:39 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+#your token here (get it on https://discordapp.com/developers)
+token = 'your token'
+
+# There has to be role named as "Muted"
+ROLES = {
+    "Muted": role_id
+}
+
+CHANNELS = {
+
+    "logs": channel_id #Optional
+}
